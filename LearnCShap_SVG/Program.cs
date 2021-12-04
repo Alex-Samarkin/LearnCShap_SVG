69446c68f366a96db76401bdca1dd2c2d3c4e0fe
Александр Иванович Самаркин <alexsamarkinru@gmail.com>
2021-12-05 00:27:33 +0300

    Добавьте файлы проекта.

---
+++ LearnCShap_SVG/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SVGClassLibrary;
+
+namespace LearnCShap_SVG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SVGDoc.Intro);
+            var key = Console.ReadKey();
+        }
+    }
+}
